5721ca1b0c6be7822ddd0fbf852c731f3aa7d14b
MatthewCodes123 <84956326+MatthewCodes123@users.noreply.github.com>
2021-08-06 20:56:29 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var firstName=("Walter ");
+var lastName=("Smith");
+console.log(firstName);
+console.log(lastName);
+var fullname=firstName.concat(lastName);
+console.log(fullname);
+var number1=("10");
+var number2=("5");
+console.log(number1);
+console.log(number2);
+var number3 =(number1 + number2);
+console.log(number3);
